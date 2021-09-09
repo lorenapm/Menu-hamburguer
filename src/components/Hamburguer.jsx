dc8b03e92641926bbaf8acac4e64ce823ab6a91b
Lorena <lorena.perez@gmail.com>
2021-09-09 12:35:56 +0200

    add menu

--- src/components/Hamburguer.jsx
+++ src/components/Hamburguer.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
+import Menu from "./Menu";
 import "../styles/Hamburguer.scss";
 
 const Hamburguer = () => {
     
     const [menuOpen, setMenuOpen] = useState(false); 
     const onClick = () => {setMenuOpen(!menuOpen)};
-    
 
     return (
+        <>
         <div className="hamburguer" onClick={onClick}>
-            
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
         </div>
+        <div>
+        {menuOpen ? (
+                <Menu/>
+            ) : (true)
+            }
+        </div>
+        </>
     )
 };
 
---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Menu.scss";
+
+const Menu = () => {
+
+    return (
+        <div className="menu">
+            <a href="/">Projects</a>
+            <a href="/">About us</a>
+            <a href="/">Contact</a>
+        </div>
+    )
+}
+
+export default Menu
